ef245aa084b01c31c470d0f74adad44dcdcdb0be
Laurel Butler <laurelbutler@Laurels-MBP.attlocal.net>
2019-03-26 12:00:49 -0400

    added header to each component

---
+++ src/Folder.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Main from './Main';
+
+class Folder extends React.Component {
+    render(){
+        return(
+            <header></header>
+        )
+    }
+}
+
+export default Folder;
